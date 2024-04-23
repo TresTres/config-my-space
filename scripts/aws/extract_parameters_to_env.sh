e5fcf5f368e80b2a7f4ec3846046d7be1164f1a3
Ezra <ezra@agathos.io>
2024-04-23 14:17:02 -0400

    script for extracting parameters from SSM

--- scripts/aws/extract_parameters_to_env.sh
+++ scripts/aws/extract_parameters_to_env.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# Invoke with . ./extract_parameters_to_env.sh ENV_VAR PARMETER LOCATION
+# Extracts paremeter from AWS SSM and sets it as an environment variable
 
 
+PARAM_NAME=$1 
+PARAM_LOC=$2  # e.g. /agathos/db-manager/lee/migrator
+eval "export $PARAM_NAME=`aws ssm get-parameter --name $PARAM_LOC --with-decryption --query "Parameter.Value" --output text`"
+eval "echo $PARAM_NAME=\$$PARAM_NAME"
 
